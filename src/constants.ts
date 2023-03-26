974fad70d0dceefd40da2929132859aadd007a36
dearlsh94 <dearlsh94@gmail.com>
2023-03-26 09:31:24 +0900

    * Add Component DimWrapper

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,12 @@ export const urlMap: Map<string, string> = new Map([
   ['/', 'Home'],
   ['/intro', 'Intro'],
 ])
+
+export const gnbLinkes = [
+  { url: '/', title: 'Hoem' },
+  { url: '/intro', title: 'Intro' },
+  { url: '/write', title: 'Write' },
+  { url: '/explain', title: 'Explain' },
+  { url: '/edit', title: 'Edit' },
+  { url: '/zip', title: 'Zip' },
+]
---
+++ src/layout/DimWrapper.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import '../scss/components.scss'
+
+interface Props {
+  children: React.ReactNode
+  handleClose?: Function
+}
+
+const DimWrapper = ({ children, handleClose }: Props) => {
+  const handleDimClick = () => {
+    handleClose && handleClose()
+  }
+  return (
+    <React.Fragment>
+      <div className="dim-wrapper">
+        <div className="content">{children}</div>
+        <div className="dim" onClick={handleDimClick} />
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default DimWrapper
