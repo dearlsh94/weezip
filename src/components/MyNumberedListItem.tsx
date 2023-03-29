3dbe233c894259aea297f2cebf7272a4ddd165f7
dearlsh94 <dearlsh94@gmail.com>
2023-03-29 21:53:43 +0900

    * Modify Style and Add Children in MyNumberedList

--- src/components/MyNumberedListItem.tsx
+++ src/components/MyNumberedListItem.tsx
@@ -2,16 +2,19 @@ import * as React from 'react'
 import '../scss/components.scss'
 import Paragraph from './Paragraph'
 import { Children } from '../types'
+import ContentWrapper from '../module/ContentWrapper'
 
 interface Props {
   numberedListItem: Children
 }
 
 const MyNumberedListItem = ({ numberedListItem }: Props) => {
+  const { has_children, children } = numberedListItem
   return (
     <React.Fragment>
-      <div className={`block-numbered-list`}>
+      <div className={`block-numbered-list-item`}>
         <Paragraph paragraph={numberedListItem.numbered_list_item} />
+        {has_children && children?.length > 0 && <ContentWrapper childrens={children} />}
       </div>
     </React.Fragment>
   )
