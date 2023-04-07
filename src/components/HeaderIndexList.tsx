c385b24bcc684005620cd7a045b3dd807876f7da
dearlsh94 <dearlsh94@gmail.com>
2023-04-07 21:53:29 +0900

    * Add Scroll Index Box to the right

--- src/components/HeaderIndexList.tsx
+++ src/components/HeaderIndexList.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
+import { useEffect, useState } from 'react'
 import '../scss/components.scss'
-import { HeaderIndex } from '../types'
+import { throttle } from '../utils/commonUtils'
 
 interface Props {
   list: HTMLHeadingElement[]
@@ -14,6 +15,30 @@ const HeaderIndexList = ({ list }: Props) => {
       behavior: 'smooth',
     })
   }
+  const moveTop = () => [
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    }),
+  ]
+  const [status, setStatus] = useState('')
+
+  useEffect(() => {
+    const scrollHandler = () => {
+      if (window.scrollY > window.innerHeight * 1.2) {
+        setStatus('scroll')
+      } else {
+        setStatus('hidden')
+      }
+    }
+    const throttledScrollHandler = throttle(scrollHandler)
+
+    window.addEventListener('scroll', throttledScrollHandler)
+    return () => {
+      window.removeEventListener('scroll', throttledScrollHandler)
+    }
+  }, [])
+
   return (
     <React.Fragment>
       <ul className={`block-header-index-list`}>
@@ -22,6 +47,18 @@ const HeaderIndexList = ({ list }: Props) => {
           const text = item.outerText
           return (
             <li className={`tag-${tag}`} key={`header-index-item-${i}`} onClick={() => move(item)}>
+              {text}
+            </li>
+          )
+        })}
+      </ul>
+      <ul className={`block-header-index-list right ${status}`}>
+        <li onClick={() => moveTop()}>처음으로</li>
+        {list.map((item, i) => {
+          const tag = item.tagName.toLowerCase()
+          const text = item.outerText
+          return (
+            <li className={`tag-${tag}`} key={`header-index-scroll-item-${i}`} onClick={() => move(item)}>
               {text}
             </li>
           )
