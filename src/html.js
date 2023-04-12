ded4638e22e039a08e9c271c622a796d6e0501f8
dearlsh94 <dearlsh94@gmail.com>
2023-04-12 21:01:55 +0900

    * Add html.js

---
+++ src/html.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <title>WeeZip</title>
+        <meta name="description" content="글쓰는 프론트엔드 개발자의 블로그" />
+        <meta name="og:description" content="글쓰는 프론트엔드 개발자의 블로그" />
+        <meta name="author" content="WeeZip.Ethan" />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  )
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
