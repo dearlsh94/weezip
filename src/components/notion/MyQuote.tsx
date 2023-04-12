a874b9ab1eaddb5bc9689e4c5f0c006f20bc9fa4
dearlsh94 <dearlsh94@gmail.com>
2023-04-12 20:33:26 +0900

    * Modify Quote Element Type

--- src/components/notion/MyQuote.tsx
+++ src/components/notion/MyQuote.tsx
@@ -11,9 +11,9 @@ const MyQuote = ({ quote }: Props) => {
   return (
     <React.Fragment>
       {quote && (
-        <div className={`block-quote`}>
+        <blockquote className={`block-quote`}>
           <Paragraph paragraph={quote} />
-        </div>
+        </blockquote>
       )}
     </React.Fragment>
   )
