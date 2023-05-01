105f7e5043fc315c37282a7ffc25ff8317b816f2
dearlsh94 <dearlsh94@gmail.com>
2023-05-01 14:59:24 +0900

    * Adapt Page Query in post page

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -2,30 +2,31 @@ import * as React from 'react'
 import { useEffect, useState } from 'react'
 import type { HeadFC, PageProps } from 'gatsby'
 import '@scss/page.scss'
-import { useGetNotionQuery } from '@services/use-notion'
+import { parseNotion } from '@services/use-notion'
 import { findContentNode } from '@utils/notionUtils'
 import MainLayout from '@layout/MainLayout'
-import { NotionContext, PageContext } from '@store/rootStore'
+import { NotionContext } from '@store/rootStore'
 import { INotionContext } from '@types'
 import SEO from '@components/header/SEO'
 import { Children } from '@types'
 import ContentWrapper from '@module/ContentWrapper'
 import HeaderIndexList from '@components/HeaderIndexList'
 import TagBadges from '@components/TagBadges'
+import { graphql } from 'gatsby'
 
-export const Head: HeadFC = ({ params }) => {
-  const nodes = useGetNotionQuery()
+export const Head: HeadFC = ({ data, params }) => {
+  const nodes = parseNotion(data)
   const content: Children | null = findContentNode(nodes, `/post/${params?.id}`)
   const title = content?.properties?.remark.rich_text || ''
   return <SEO title={title} description={content?.properties?.series.rich_text}></SEO>
 }
 
-const ListPage: React.FC<PageProps> = (props: PageProps) => {
-  const nodes = useGetNotionQuery()
+const ListPage: React.FC<PageProps> = ({ data, params }) => {
+  const { id } = params
+  const nodes = parseNotion(data)
   const store: INotionContext = {
     nodes: nodes,
   }
-  const { id } = props?.params
   const content: Children | null = findContentNode(nodes, `/post/${id}`)
   const title = content?.properties?.remark.rich_text || ''
   const [indexList, setIndexList] = useState<HTMLHeadingElement[]>([])
@@ -42,31 +43,80 @@ const ListPage: React.FC<PageProps> = (props: PageProps) => {
   }, [])
 
   return (
-    <PageContext.Provider value={props}>
-      <NotionContext.Provider value={store}>
-        <MainLayout className="post-layout">
-          <div>
-            <TagBadges tag={content?.properties.tag} />
+    <NotionContext.Provider value={store}>
+      <MainLayout className="post-layout">
+        <div>
+          <TagBadges tag={content?.properties.tag} />
+        </div>
+        <div className="title-box">
+          {content?.properties?.series.rich_text && (
+            <span className={`series-title`}>[{content?.properties?.series.rich_text}]</span>
+          )}
+          <h1 className="title">{title}</h1>
+          <div className="desc-box">
+            <span className="date">작성일 : {content?.properties?.created_date?.date.start}</span>
+            <span className="date">수정일 : {content?.properties?.edited_date?.date.start}</span>
           </div>
-          <div className="title-box">
-            {content?.properties?.series.rich_text && (
-              <span className={`series-title`}>[{content?.properties?.series.rich_text}]</span>
-            )}
-            <h1 className="title">{title}</h1>
-            <div className="desc-box">
-              <span className="date">작성일 : {content?.properties?.created_date?.date.start}</span>
-              <span className="date">수정일 : {content?.properties?.edited_date?.date.start}</span>
-            </div>
-          </div>
-          <div className="index-box">
-            <p>목차</p>
-            {indexList && indexList?.length > 0 && <HeaderIndexList list={indexList} />}
-          </div>
-          {content && <ContentWrapper childrens={content.children} />}
-        </MainLayout>
-      </NotionContext.Provider>
-    </PageContext.Provider>
+        </div>
+        <div className="index-box">
+          <p>목차</p>
+          {indexList && indexList?.length > 0 && <HeaderIndexList list={indexList} />}
+        </div>
+        {content && <ContentWrapper childrens={content.children} />}
+      </MainLayout>
+    </NotionContext.Provider>
   )
 }
+
+export const postQuery = graphql`
+  query {
+    allNotion {
+      edges {
+        node {
+          archived
+          children {
+            id
+          }
+          createdAt
+          id
+          internal {
+            content
+          }
+          json
+          markdownString
+          parent {
+            id
+            internal {
+              content
+            }
+          }
+          raw {
+            archived
+            children {
+              id
+            }
+            created_by {
+              id
+            }
+            created_time
+            id
+            last_edited_by {
+              id
+            }
+            last_edited_time
+            object
+            parent {
+              database_id
+              type
+            }
+            url
+          }
+          title
+          updatedAt
+        }
+      }
+    }
+  }
+`
 
 export default ListPage
