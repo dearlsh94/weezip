a1b5521679a8915cb7bb3a0d9a6c50ded93024c8
dearlsh94 <dearlsh94@gmail.com>
2023-07-06 15:26:05 +0900

    Fix : Render Callout children

--- src/components/notion/MyCallout.tsx
+++ src/components/notion/MyCallout.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import '@scss/notion.scss'
-import { Callout } from '@types'
+import { Callout, Children } from '@types'
 import Paragraph from '@components/notion/Paragraph'
+import ContentWrapper from '@module/ContentWrapper'
 interface Props {
   callout: Callout
+  children: Children[]
 }
 
-const MyCallout = ({ callout }: Props) => {
+const MyCallout = ({ callout, children = [] }: Props) => {
   const icon = callout?.icon?.emoji
 
   return (
@@ -15,6 +17,7 @@ const MyCallout = ({ callout }: Props) => {
         <div className={`block-callout`}>
           {icon && <div className="icon-box">{icon}</div>}
           <Paragraph paragraph={callout} />
+          {children && children?.length > 0 && <ContentWrapper childrens={children} />}
         </div>
       )}
     </React.Fragment>
