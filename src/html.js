c59dc8909b4cf889ada9473362d827aaf33b2b87
dearlsh94 <dearlsh94@gmail.com>
2023-07-29 15:39:24 +0900

    Add : noscript

--- src/html.js
+++ src/html.js
@@ -18,6 +18,16 @@ export default function HTML(props) {
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
+        <noscript>
+          이 웹사이트는 자바스크립트를 지원하지 않는 브라우저에서 제한적인 기능으로 동작할 수 있습니다.
+          <br />
+          웹 브라우저 설정에서 자바스크립트를 활성화해주세요.
+          <br />
+          <br />
+          This website may function with limited functionality in browsers that do not support JavaScript.
+          <br />
+          Please enable JavaScript in your web browser settings.
+        </noscript>
         {props.preBodyComponents}
         <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
         {props.postBodyComponents}
