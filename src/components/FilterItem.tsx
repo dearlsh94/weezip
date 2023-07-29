869c759f7b1e46cd0be19a0dadb996a03def8c35
dearlsh94 <dearlsh94@gmail.com>
2023-07-29 15:17:48 +0900

    * Modify : FilterItem Link Logic

--- src/components/FilterItem.tsx
+++ src/components/FilterItem.tsx
@@ -3,6 +3,7 @@ import { useLocation } from '@reach/router'
 import '@scss/components/FilterItem.scss'
 import { Filter } from '@types'
 import { navigate } from 'gatsby'
+import Linker from './ui/Linker'
 
 interface Props {
   filter: Filter
@@ -20,14 +21,11 @@ const FilterItem = ({ filter }: Props) => {
     }
   }, [location])
 
-  const handelFilter = () => {
-    navigate(`/list?${filter.type}=${encodeURIComponent(filter.key)}`)
-  }
   return (
     <React.Fragment>
-      <div className={`filter-item ${filter.color} ${isActive ? 'active' : ''}`} onClick={handelFilter}>
-        {filter.name}
-      </div>
+      <Linker url={`/list?${filter.type}=${encodeURIComponent(filter.key)}`}>
+        <div className={`filter-item ${filter.color} ${isActive ? 'active' : ''}`}>{filter.name}</div>
+      </Linker>
     </React.Fragment>
   )
 }
