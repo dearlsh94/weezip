eb1f2c1cbc3117359e8635a624d5d7620d39db24
dearlsh94 <dearlsh94@gmail.com>
2023-07-29 14:28:49 +0900

    Modify : TagBadge Component Style

--- src/components/TagBadges.tsx
+++ src/components/TagBadges.tsx
@@ -1,23 +1,29 @@
 import * as React from 'react'
 import '@scss/components/TagBadge.scss'
-import { Property, Select } from '@types'
+import { Select } from '@types'
+import Linker from './ui/Linker'
 
 interface Props {
   postItemTags?: Select[]
+  isLink?: boolean
 }
 
-const TagBadges = ({ postItemTags }: Props) => {
+const TagBadges = ({ postItemTags, isLink = true }: Props) => {
   return (
     <React.Fragment>
       {postItemTags && (
         <div className="badge-box">
-          {postItemTags.map((t, i) => {
-            return (
-              <span className={`badge ${t.color}-border`} key={`t-badge-${i}`}>
-                #{t.name}
+          {postItemTags.map((tag, i) =>
+            isLink ? (
+              <Linker url={`/list?tag=${tag.name}`} key={`tag-badge-${i}`}>
+                <span className={`badge linked`}>#{tag.name}</span>
+              </Linker>
+            ) : (
+              <span className={`badge`} key={`tag-badge-${i}`}>
+                #{tag.name}
               </span>
             )
-          })}
+          )}
         </div>
       )}
     </React.Fragment>
