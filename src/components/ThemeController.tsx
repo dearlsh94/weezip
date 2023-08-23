f61d50c8ce0c1f19b4b58f5dbb3ad1242ce5b4a9
dearlsh94 <dearlsh94@gmail.com>
2023-08-23 17:09:39 +0900

    Modify : theme config

--- src/components/ThemeController.tsx
+++ src/components/ThemeController.tsx
@@ -1,18 +1,30 @@
 import * as React from 'react'
-// import '@scss/components.scss'
 import IconLightMode from './icon/IconLightTheme'
 import IconDarkTheme from './icon/IconDarkTheme'
+import { getConfig, setConfig } from '@utils/configUtils'
 
 enum THEME_MODE {
   LIGHT = 'light',
   DARK = 'dark',
 }
-const STORAGE_KEY = 'weezip-theme'
+const CONFIG_KEY = 'weezip-theme'
 
 const StyleMode = () => {
   const [theme, setTheme] = React.useState<THEME_MODE>()
+  const setLightMode = () => {
+    document.documentElement.setAttribute(CONFIG_KEY, THEME_MODE.LIGHT)
+    setConfig(CONFIG_KEY, THEME_MODE.LIGHT)
+    setTheme(THEME_MODE.LIGHT)
+  }
+
+  const setDarkMode = () => {
+    document.documentElement.setAttribute(CONFIG_KEY, THEME_MODE.DARK)
+    setConfig(CONFIG_KEY, THEME_MODE.DARK)
+    setTheme(THEME_MODE.DARK)
+  }
   React.useEffect(() => {
-    const theme = localStorage.getItem(STORAGE_KEY)
+    const theme = getConfig(CONFIG_KEY)
+    console.log(theme)
     if (theme === THEME_MODE.DARK) {
       setDarkMode()
       setTheme(THEME_MODE.DARK)
@@ -21,18 +33,6 @@ const StyleMode = () => {
       setTheme(THEME_MODE.LIGHT)
     }
   }, [])
-
-  const setLightMode = () => {
-    document.documentElement.setAttribute(STORAGE_KEY, THEME_MODE.LIGHT)
-    localStorage.setItem(STORAGE_KEY, THEME_MODE.LIGHT)
-    setTheme(THEME_MODE.LIGHT)
-  }
-
-  const setDarkMode = () => {
-    document.documentElement.setAttribute(STORAGE_KEY, THEME_MODE.DARK)
-    localStorage.setItem(STORAGE_KEY, THEME_MODE.DARK)
-    setTheme(THEME_MODE.DARK)
-  }
 
   const handleChange = () => {
     if (theme === THEME_MODE.DARK) {
