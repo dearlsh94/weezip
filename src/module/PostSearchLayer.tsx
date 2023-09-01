db5c1bf711a2b5b687391d021c129eb717db2760
dearlsh94 <dearlsh94@gmail.com>
2023-09-01 15:51:34 +0900

    Refactor : PostSearchLayer use portal, divide component

---
+++ src/module/PostSearchLayer.tsx
@@ -0,0 +1,78 @@
+import * as React from 'react'
+import { useRef, useState } from 'react'
+import '@scss/module/PostSearchLayer.scss'
+import { throttle } from '@utils/commonUtils'
+import { IconClose, IconSearch } from '@components/icon'
+import { navigate } from 'gatsby'
+import RecommendTag from '../components/RecommendTag'
+import Dim from '@layout/Dim'
+import { GlobalPortal } from '@components/GlobalPortal'
+
+interface PostSearchLayerProps {
+  handleClose: () => void
+}
+
+const PostSearchLayer = ({ handleClose }: PostSearchLayerProps) => {
+  const inputRef = useRef<HTMLInputElement | null>(null)
+  const [searchText, setSearchText] = useState('')
+
+  React.useEffect(() => {
+    inputRef?.current?.focus()
+  }, [])
+
+  const clear = () => {
+    setSearchText('')
+  }
+
+  const search = throttle(() => {
+    navigate(`/list?keyword=${encodeURIComponent(searchText)}`)
+    handleClose()
+  }, 300)
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      search()
+    } else if (e.key === 'Escape') {
+      e.preventDefault()
+      clear()
+    }
+  }
+
+  return (
+    <GlobalPortal.Consumer>
+      <div className="post-search-container">
+        <div className="post-search-box">
+          <div className="icon-close-box">
+            <IconClose size={32} handleClick={handleClose} />
+          </div>
+          <div className="search-box">
+            <div className="input-box">
+              <input
+                ref={inputRef}
+                className="search"
+                type="text"
+                placeholder="검색어를 입력하세요."
+                value={searchText}
+                onChange={e => setSearchText(e.target.value)}
+                onKeyDown={handleKeyDown}
+              />
+              {searchText && (
+                <div className="icon-clear-box">
+                  <IconClose size={28} handleClick={clear} />
+                </div>
+              )}
+            </div>
+            <div className="icon-search-box">
+              <IconSearch size={28} handleClick={search} />
+            </div>
+          </div>
+          <RecommendTag />
+        </div>
+      </div>
+      <Dim handleClose={handleClose} />
+    </GlobalPortal.Consumer>
+  )
+}
+
+export default PostSearchLayer
