21b548ba8d33856f0f118a4d68f78614da178b1f
dearlsh94 <dearlsh94@gmail.com>
2023-09-01 15:29:57 +0900

    Refactor : SNB Portal

--- src/module/SideBarNavigation.tsx
+++ src/module/SideBarNavigation.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import { useContext } from 'react'
 import '@scss/module/SideBarNavigation.scss'
 import { NotionContext } from '@store/rootStore'
-import { GlobalPortal } from '@components/GlobalPortal'
 import SideBarLayout from '@layout/SideBarLayout'
 import { StaticImage } from 'gatsby-plugin-image'
 import { isDebug, GNB_MENUS } from '@src/constants'
@@ -18,54 +17,52 @@ const SideBarNavigation = ({ handleClose }: SideBarNavigationProps) => {
   const nodes: NotionNode[] = useContext(NotionContext).nodes
 
   return (
-    <GlobalPortal.Consumer>
-      <SideBarLayout handleClose={handleClose}>
-        <div className="snb-header">
-          <StaticImage src={`../images/logo-3x.png`} alt="Weezip Logo" className="logo" />
-          <div className="text-box">
-            {/* TODO 무슨 문구를 써놓을까 */}
-            <p></p>
-          </div>
+    <SideBarLayout handleClose={handleClose}>
+      <div className="snb-header">
+        <StaticImage src={`../images/logo-3x.png`} alt="Weezip Logo" className="logo" />
+        <div className="text-box">
+          {/* TODO 무슨 문구를 써놓을까 */}
+          <p></p>
         </div>
-        <nav className="snb-box">
-          <ul>
-            {GNB_MENUS?.length > 0 &&
-              GNB_MENUS.map((nav, i) => {
-                return (
-                  <li key={`gnb-${i}`} className={`nav-item`}>
-                    <Linker url={nav.url} target={nav.isOutLink ? '_blank' : '_parent'}>
-                      <div className="title-box">
-                        {nav.title.toUpperCase() === 'HOME' && <IconHome />}
-                        {nav.title.toUpperCase() === 'LIST' && <IconList />}
-                        {nav.title.toUpperCase() === '문화소비자시점' && <IconStar />}
-                        {nav.isOutLink && <IconOutLink />}
-                        <span>{nav.title}</span>
-                      </div>
-                    </Linker>
-                  </li>
-                )
-              })}
-          </ul>
-        </nav>
-        {isDebug && (
-          <section>
-            {nodes.map((node: NotionNode) => {
+      </div>
+      <nav className="snb-box">
+        <ul>
+          {GNB_MENUS?.length > 0 &&
+            GNB_MENUS.map((nav, i) => {
               return (
-                <div key={`node-${node.id}`}>
-                  <a href={`${node.title || location.pathname}`}>
-                    <p>
-                      <span>id: {node.id}</span>
-                      <span>&nbsp;/&nbsp;</span>
-                      <span>title: {node.title}</span>
-                    </p>
-                  </a>
-                </div>
+                <li key={`gnb-${i}`} className={`nav-item`}>
+                  <Linker url={nav.url} target={nav.isOutLink ? '_blank' : '_parent'}>
+                    <div className="title-box">
+                      {nav.title.toUpperCase() === 'HOME' && <IconHome />}
+                      {nav.title.toUpperCase() === 'LIST' && <IconList />}
+                      {nav.title.toUpperCase() === '문화소비자시점' && <IconStar />}
+                      {nav.isOutLink && <IconOutLink />}
+                      <span>{nav.title}</span>
+                    </div>
+                  </Linker>
+                </li>
               )
             })}
-          </section>
-        )}
-      </SideBarLayout>
-    </GlobalPortal.Consumer>
+        </ul>
+      </nav>
+      {isDebug && (
+        <section>
+          {nodes.map((node: NotionNode) => {
+            return (
+              <div key={`node-${node.id}`}>
+                <a href={`${node.title || location.pathname}`}>
+                  <p>
+                    <span>id: {node.id}</span>
+                    <span>&nbsp;/&nbsp;</span>
+                    <span>title: {node.title}</span>
+                  </p>
+                </a>
+              </div>
+            )
+          })}
+        </section>
+      )}
+    </SideBarLayout>
   )
 }
 
