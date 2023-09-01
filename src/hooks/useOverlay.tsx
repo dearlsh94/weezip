154274bf55920095ab5f0399eb21427ef6f239c3
dearlsh94 <dearlsh94@gmail.com>
2023-09-01 15:15:33 +0900

    Add : useOverlay hook

---
+++ src/hooks/useOverlay.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+
+const useOverlay = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+  }, [isOpen])
+
+  return {
+    isOpen,
+    open: () => setIsOpen(true),
+    close: () => setIsOpen(false),
+  }
+}
+
+export default useOverlay
