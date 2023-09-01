de6ebc4c86fb5555c24d46ececcf7c8f9aa2d664
dearlsh94 <dearlsh94@gmail.com>
2023-09-01 15:29:57 +0900

    Refactor : SNB Portal

--- src/layout/SideBarLayout.tsx
+++ src/layout/SideBarLayout.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { ReactNode } from 'react'
 import Dim from '@layout/Dim'
 import '@scss/layout/SideBarLayout.scss'
+import { GlobalPortal } from '@components/GlobalPortal'
 
 interface SideBarLayoutProps {
   children: ReactNode
@@ -10,12 +11,12 @@ interface SideBarLayoutProps {
 
 const SideBarLayout = ({ handleClose, children }: SideBarLayoutProps) => {
   return (
-    <>
+    <GlobalPortal.Consumer>
       <aside className={`sidebar-layout`}>
         <div className="content">{children}</div>
       </aside>
       <Dim handleClose={handleClose} />
-    </>
+    </GlobalPortal.Consumer>
   )
 }
 
