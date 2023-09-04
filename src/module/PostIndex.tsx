4178b2eaba03d6e43bbcde33483427f781bab47c
dearlsh94 <dearlsh94@gmail.com>
2023-09-04 16:08:26 +0900

    Refactor : PostIndex

--- src/module/PostIndex.tsx
+++ src/module/PostIndex.tsx
@@ -9,20 +9,17 @@ interface PostIndexProps {
 }
 
 const PostIndex = ({ list }: PostIndexProps) => {
-  const showIndex = useShow()
+  const showPostIndex = useShow()
 
-  const handleShowIndex = () => {
-    showIndex.change()
-  }
   return (
     <div className="post-index-container">
-      <div className="title-box" onClick={handleShowIndex}>
-        <p>목차 {showIndex.isShow ? '숨기기' : '보기'}</p>
-        <div className={`icon-box ${showIndex.isShow ? '' : 'hide'}`}>
+      <div className="title-box" onClick={showPostIndex.change}>
+        <p>목차 {showPostIndex.isShow ? '숨기기' : '보기'}</p>
+        <div className={`icon-box ${showPostIndex.isShow ? '' : 'hide'}`}>
           <IconArrow direction={'top'} size={20} />
         </div>
       </div>
-      <div className={`post-index-box ${showIndex.isShow ? '' : 'hide'}`}>
+      <div className={`post-index-box ${showPostIndex.isShow ? '' : 'hide'}`}>
         {list && list?.length > 0 && <PostIndexList list={list} />}
       </div>
     </div>
