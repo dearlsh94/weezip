c199cfb15fcc8f0db74b69e41a0fbce4bf8144a8
dearlsh94 <dearlsh94@gmail.com>
2023-09-10 13:16:30 +0900

    Create : PostIndex in SideBarLayout (handle active state)

--- src/components/PostIndexList.tsx
+++ src/components/PostIndexList.tsx
@@ -1,12 +1,37 @@
 import * as React from 'react'
 import '@scss/components/PostIndexList.scss'
+import useScroll from '@src/hooks/useScroll'
 
 interface PostIndexListProps {
   list: HTMLHeadingElement[]
   useMoveTop?: boolean
+  useActive?: boolean
 }
 
-const PostIndexList = ({ list, useMoveTop = false }: PostIndexListProps) => {
+const PostIndexList = ({ list, useMoveTop = false, useActive = false }: PostIndexListProps) => {
+  const [activeIndexText, setActiveIndexText] = React.useState('')
+
+  if (useActive) {
+    const { scrollY } = useScroll()
+    const offsetTopPositions = list.map(i => {
+      return {
+        offsetTop: i.offsetTop,
+        name: i.outerText,
+      }
+    })
+
+    React.useEffect(() => {
+      setActiveIndexText(
+        offsetTopPositions.find((item, i) => {
+          const activeLine = item.offsetTop - 300
+          const nextOffsetTop = offsetTopPositions[i + 1]?.offsetTop
+
+          return nextOffsetTop ? activeLine < scrollY && scrollY < nextOffsetTop - 200 : activeLine < scrollY
+        })?.name || ''
+      )
+    }, [scrollY])
+  }
+
   const move = (item: HTMLHeadingElement) => {
     window.scrollTo({
       top: item.offsetTop,
@@ -24,7 +49,7 @@ const PostIndexList = ({ list, useMoveTop = false }: PostIndexListProps) => {
   return (
     <ul className={`block-header-index-list`}>
       {useMoveTop && (
-        <li className="tag-top" onClick={moveTop}>
+        <li id={`index-top`} className="tag-top" onClick={moveTop}>
           맨위로
         </li>
       )}
@@ -32,7 +57,12 @@ const PostIndexList = ({ list, useMoveTop = false }: PostIndexListProps) => {
         const tag = item.tagName.toLowerCase()
         const text = item.outerText
         return (
-          <li className={`tag-${tag}`} key={`header-index-item-${i}`} onClick={() => move(item)}>
+          <li
+            id={`index-${text}`}
+            className={`tag-${tag} ${activeIndexText === text ? 'active' : ''}`}
+            key={`header-index-item-${i}`}
+            onClick={() => move(item)}
+          >
             {text}
           </li>
         )
--- src/module/PostIndex.tsx
+++ src/module/PostIndex.tsx
@@ -3,16 +3,18 @@ import '@scss/module/PostIndex.scss'
 import PostIndexList from '@components/PostIndexList'
 import { IconArrow } from '@components/icon'
 import useShow from '@src/hooks/useShow'
-
+import SideBarLayout from '@layout/SideBarLayout'
+import useResize from '@src/hooks/useResize'
 interface PostIndexProps {
   list: HTMLHeadingElement[]
 }
 
 const PostIndex = ({ list }: PostIndexProps) => {
   const showPostIndex = useShow()
+  const { resizedInnerWidth } = useResize()
 
   return (
-    <div className="post-index-container">
+    <section className="post-index-container">
       <div className="title-box" onClick={showPostIndex.change}>
         <p>목차 {showPostIndex.isShow ? '숨기기' : '보기'}</p>
         <div className={`icon-box ${showPostIndex.isShow ? '' : 'hide'}`}>
@@ -22,7 +24,14 @@ const PostIndex = ({ list }: PostIndexProps) => {
       <div className={`post-index-box ${showPostIndex.isShow ? '' : 'hide'}`}>
         {list && list?.length > 0 && <PostIndexList list={list} />}
       </div>
-    </div>
+      {resizedInnerWidth > 1280 && (
+        <SideBarLayout handleClose={() => {}} useDim={false}>
+          <div className="side-post-index">
+            <PostIndexList list={list} useMoveTop={true} useActive={true} />
+          </div>
+        </SideBarLayout>
+      )}
+    </section>
   )
 }
 
