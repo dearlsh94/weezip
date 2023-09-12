88527fc5ec2f93dfdfeb7480cd3166ac574e7936
dearlsh94 <dearlsh94@gmail.com>
2023-09-12 21:16:14 +0900

    Modify : ListFilter Sort

--- src/components/ListFilter.tsx
+++ src/components/ListFilter.tsx
@@ -12,19 +12,6 @@ const ListFilter = () => {
 
   return (
     <div className="list-filter-container">
-      <div className="series filter">
-        <p className="title">시리즈</p>
-        <div className="filter-box">
-          {postSeries?.map(series => {
-            return (
-              <React.Fragment key={`series-filter-item-${series.name}`}>
-                <FilterLinker type="series" name={series.name} color={series.color} />
-              </React.Fragment>
-            )
-          })}
-        </div>
-      </div>
-      <Divider />
       <div className="category filter">
         <p className="title">카테고리</p>
         <div className="filter-box">
@@ -32,6 +19,19 @@ const ListFilter = () => {
             return (
               <React.Fragment key={`category-filter-item-${filter.key}`}>
                 <FilterLinker type={filter.type} name={filter.name} color={filter.color} />
+              </React.Fragment>
+            )
+          })}
+        </div>
+      </div>
+      <Divider />
+      <div className="series filter">
+        <p className="title">시리즈</p>
+        <div className="filter-box">
+          {postSeries?.map(series => {
+            return (
+              <React.Fragment key={`series-filter-item-${series.name}`}>
+                <FilterLinker type="series" name={series.name} color={series.color} />
               </React.Fragment>
             )
           })}
