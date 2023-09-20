a0a3a84b55d12a8e8965bb80fafeda6f540c9aa5
dearlsh94 <dearlsh94@gmail.com>
2023-09-20 21:10:56 +0900

    Modify : useTheme hook 적용

--- src/components/ThemeController.tsx
+++ src/components/ThemeController.tsx
@@ -1,53 +1,12 @@
 import * as React from 'react'
-import IconLightMode from './icon/IconLightTheme'
+import IconLightTheme from './icon/IconLightTheme'
 import IconDarkTheme from './icon/IconDarkTheme'
-import { getConfig, setConfig } from '@utils/configUtils'
-import { CONFIG_THEME_KEY } from '@src/constants'
+import useTheme from '@src/hooks/useTheme'
 
-enum THEME_MODE {
-  LIGHT = 'light',
-  DARK = 'dark',
+const ThemeController = () => {
+  const { theme, setLight, setDark } = useTheme()
+
+  return <>{theme === 'light' ? <IconLightTheme handleClick={setDark} /> : <IconDarkTheme handleClick={setLight} />}</>
 }
 
-const StyleMode = () => {
-  const [theme, setTheme] = React.useState<THEME_MODE>()
-  const setLightMode = () => {
-    document.documentElement.setAttribute(CONFIG_THEME_KEY, THEME_MODE.LIGHT)
-    setConfig(CONFIG_THEME_KEY, THEME_MODE.LIGHT)
-    setTheme(THEME_MODE.LIGHT)
-  }
-
-  const setDarkMode = () => {
-    document.documentElement.setAttribute(CONFIG_THEME_KEY, THEME_MODE.DARK)
-    setConfig(CONFIG_THEME_KEY, THEME_MODE.DARK)
-    setTheme(THEME_MODE.DARK)
-  }
-  React.useEffect(() => {
-    const theme = getConfig(CONFIG_THEME_KEY)
-    if (theme === THEME_MODE.DARK) {
-      setDarkMode()
-      setTheme(THEME_MODE.DARK)
-    } else {
-      setLightMode()
-      setTheme(THEME_MODE.LIGHT)
-    }
-  }, [])
-
-  const handleChange = () => {
-    if (theme === THEME_MODE.DARK) {
-      setLightMode()
-    } else if (theme === THEME_MODE.LIGHT) {
-      setDarkMode()
-    } else {
-      setDarkMode()
-    }
-  }
-
-  return (
-    <>
-      {theme === 'light' ? <IconLightMode handleClick={handleChange} /> : <IconDarkTheme handleClick={handleChange} />}
-    </>
-  )
-}
-
-export default StyleMode
+export default ThemeController
