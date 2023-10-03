e70c0cd0f44548e1fd76ac81fd70ba2ed613819e
dearlsh94 <dearlsh94@gmail.com>
2023-10-03 19:00:16 +0900

    Modify : constants freeze

--- src/constants.ts
+++ src/constants.ts
@@ -2,14 +2,14 @@ import { Filter } from './types';
 
 export const isDebug = false;
 
-export const GNB_MENUS = [
+export const GNB_MENUS = Object.freeze([
   { url: '/', title: 'Home', isOutLink: false },
   { url: '/list', title: 'List', isOutLink: false },
   { url: '/list?series=review', title: '문화소비자시점', isOutLink: false },
   { url: 'https://treefeely.com', title: 'Treefeely', isOutLink: true },
-];
+]);
 
-export const CATEGORY_FILTERS: Filter[] = [
+export const CATEGORY_FILTERS: readonly Filter[] = Object.freeze([
   {
     type: 'category',
     key: 'write',
@@ -34,9 +34,9 @@ export const CATEGORY_FILTERS: Filter[] = [
     name: 'Zip',
     color: 'blue',
   },
-];
+]);
 
-export const RECOMMEND_TAGS = [
+export const RECOMMEND_TAGS = Object.freeze([
   {
     url: 'list/?series=review',
     name: '문화소비자시점',
@@ -49,7 +49,7 @@ export const RECOMMEND_TAGS = [
     url: 'list/?tag=essay',
     name: 'Essay',
   },
-];
+]);
 
-export const OWNER_EMAIL = 'weezip.ethan@gmail.com';
-export const CONFIG_THEME_KEY = 'weezip-theme';
+export const OWNER_EMAIL = Object.freeze('weezip.ethan@gmail.com');
+export const CONFIG_THEME_KEY = Object.freeze('weezip-theme');
