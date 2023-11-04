c87fc0f7dbf516fd2520e8703e65094e5ea0d1da
dearlsh94 <dearlsh94@gmail.com>
2023-11-04 14:42:02 +0900

    Remove : 카테고리 필터

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,3 @@
-import { Filter } from './types';
-
 export const isDebug = false;
 
 export const GNB_MENUS = Object.freeze([
@@ -7,33 +5,6 @@ export const GNB_MENUS = Object.freeze([
   { url: '/list', title: 'List', isOutLink: false },
   { url: '/list?series=review', title: '문화소비자시점', isOutLink: false },
   { url: 'https://treefeely.com', title: 'Treefeely', isOutLink: true },
-]);
-
-export const CATEGORY_FILTERS: readonly Filter[] = Object.freeze([
-  {
-    type: 'category',
-    key: 'write',
-    name: 'Write',
-    color: 'brown',
-  },
-  {
-    type: 'category',
-    key: 'explain',
-    name: 'Explain',
-    color: 'orange',
-  },
-  {
-    type: 'category',
-    key: 'edit',
-    name: 'Edit',
-    color: 'yellow',
-  },
-  {
-    type: 'category',
-    key: 'zip',
-    name: 'Zip',
-    color: 'blue',
-  },
 ]);
 
 export const RECOMMEND_TAGS = Object.freeze([
