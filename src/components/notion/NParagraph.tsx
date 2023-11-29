4536e1e0d88f6f02e0d6184cbc9206ca102eceef
dearlsh94 <dearlsh94@gmail.com>
2023-11-29 14:18:09 +0900

    Refactor : Paragraph tag

--- src/components/notion/NParagraph.tsx
+++ src/components/notion/NParagraph.tsx
@@ -29,48 +29,49 @@ export default function NParagraph({ paragraph, richText, className }: NParagrap
             if (t?.href) {
               return (
                 <Linker key={`block-paragraph-text-${i}`} url={t.href} target="_blank" aria-label={`링크 텍스트`}>
-                  {t.plain_text.replaceAll('\n', '<br/>')}
+                  {t.plain_text}
                 </Linker>
               );
             }
             if (t?.annotations?.bold) {
               return (
                 <b key={`block-paragraph-text-${i}`} className={classNames.join(' ')}>
-                  {t.plain_text.replaceAll('\n', '<br/>')}
+                  {t.plain_text}
                 </b>
               );
             }
             if (t?.annotations?.italic) {
               return (
                 <i key={`block-paragraph-text-${i}`} className={classNames.join(' ')}>
-                  {t.plain_text.replaceAll('\n', '<br/>')}
+                  {t.plain_text}
                 </i>
               );
             }
             if (t?.annotations?.strikethrough) {
               return (
                 <s key={`block-paragraph-text-${i}`} className={classNames.join(' ')}>
-                  {t.plain_text.replaceAll('\n', '<br/>')}
+                  {t.plain_text}
                 </s>
               );
             }
             if (t?.annotations?.underline) {
               return (
                 <u key={`block-paragraph-text-${i}`} className={classNames.join(' ')}>
-                  {t.plain_text.replaceAll('\n', '<br/>')}
+                  {t.plain_text}
                 </u>
               );
             }
             if (t?.annotations?.code) {
               return (
                 <code key={`block-paragraph-text-${i}`} className={classNames.join(' ')}>
-                  {t.plain_text.replaceAll('\n', '<br/>')}
+                  {t.plain_text}
                 </code>
               );
             }
+            const text = t.plain_text.replaceAll('\n', '<br/>');
             return (
               <React.Fragment key={`block-paragraph-text-${i}`}>
-                <span className={classNames.join(' ')}>{t.plain_text.replaceAll('\n', '<br/>')}</span>
+                <p className={classNames.join(' ')} dangerouslySetInnerHTML={{ __html: text }} />
               </React.Fragment>
             );
           })}
