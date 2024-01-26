333bc085a9e7e83e450fbd38a47f688ff1f8ac0e
dearlsh94 <dearlsh94@gmail.com>
2024-01-26 17:18:25 +0900

    refactor: 불필요한 주석 삭제

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,5 @@
 import type { GatsbyConfig } from 'gatsby';
 
-// Handle Environment Variables
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 });
@@ -14,7 +13,6 @@ const config: GatsbyConfig = {
     siteUrl: siteUrl,
   },
   graphqlTypegen: true,
-  // For to check not available during SSR
   flags: {
     DEV_SSR: true,
     FAST_DEV: true,
