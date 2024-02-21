18d19e4c1be6f6b9ae733ea3259bdc2c255e1f33
dearlsh94 <dearlsh94@gmail.com>
2024-02-21 21:34:19 +0900

    feat: 404페이지에 최신 포스트 보기 추가

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,21 +4,22 @@ import * as React from 'react';
 
 import { GlobalPortal } from '@components/GlobalPortal';
 import SEO from '@components/header/SEO';
+import { LatestPost } from '@components/post';
 import { Linker } from '@components/ui';
 import { ARIA_LABEL } from '@src/constants';
-
-const mainStyles = {
-  padding: '40px',
-};
-const contentStyles = {
-  margin: '20px 0px',
-};
 
 export const Head: HeadFC = () => {
   return <SEO />;
 };
 
 const NotFoundPage: React.FC<PageProps> = () => {
+  const mainStyles = {
+    padding: '40px',
+  };
+  const contentStyles = {
+    margin: '20px 0px',
+  };
+
   return (
     <GlobalPortal.Provider>
       <main style={mainStyles}>
@@ -27,11 +28,12 @@ const NotFoundPage: React.FC<PageProps> = () => {
           <p>죄송합니다. 요청하신 페이지를 찾을 수 없습니다.</p>
           <p>Sorry. we couldn’t find what you were looking for.</p>
         </div>
-        <div>
+        <div style={contentStyles}>
           <Linker label={`메인 페이지로 ${ARIA_LABEL.MOVE}`} url="/">
             Go Main
           </Linker>
         </div>
+        <LatestPost />
       </main>
     </GlobalPortal.Provider>
   );
