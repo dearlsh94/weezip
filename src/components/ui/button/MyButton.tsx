ecb74b4e88c281d3857618e1f0030ed7fe654ea7
dearlsh94 <dearlsh94@gmail.com>
2024-03-08 22:04:51 +0900

    refactor: 오타 수정

--- src/components/ui/button/MyButton.tsx
+++ src/components/ui/button/MyButton.tsx
@@ -18,14 +18,14 @@ export enum ButtonType {
   BG = 'type-bg',
 }
 
-interface MyBUttonProps extends React.PropsWithChildren<React.HTMLAttributes<HTMLButtonElement>> {
+interface MyButtonProps extends React.PropsWithChildren<React.HTMLAttributes<HTMLButtonElement>> {
   size: ButtonSize;
   color: ButtonColor;
   type: ButtonType;
   width?: number | '100%';
 }
 
-export default function MyButton({ size, color, type, width, className, children, ...rest }: MyBUttonProps) {
+export default function MyButton({ size, color, type, width, className, children, ...rest }: MyButtonProps) {
   return (
     <button
       className={`my-button ${size} ${color} ${type} ${className}`}
