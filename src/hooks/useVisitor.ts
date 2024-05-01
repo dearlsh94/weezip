5af8285e0fff51f77ea4f32b692ea46a9cbdfb27
dearlsh94 <dearlsh94@gmail.com>
2024-05-01 17:37:04 +0900

    feat: 방문자 수 확인 기능 추가

---
+++ src/hooks/useVisitor.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+
+import { fetchAllVisitorsCount, fetchTodayVisitorsCount } from '@src/services/visitors';
+
+export const useVisitor = () => {
+  const [allVisitors, setAllVisitors] = useState<number>(0);
+  const [todayVisitors, setTodayVisitors] = useState<number>(0);
+
+  async function loadAllVisitors() {
+    const allVisitorsCount = await fetchAllVisitorsCount();
+    if (allVisitorsCount) {
+      // GA를 통해 이전 방문자 수 체크해서 소급
+      // 435 : GA 변경 전 2024.01.01 ~ 2024.02.10
+      // 752 : GA 변경 전 2023.04.01 ~ 2023.12.31
+      // 1787 : GA 변경 후 2023.04.30 ~ 2024.04.30
+      setAllVisitors(allVisitorsCount + 435 + 752 + 1787);
+    }
+  }
+
+  async function loadTodayVisitors() {
+    const todayVisitorsCount = await fetchTodayVisitorsCount();
+    if (todayVisitorsCount) {
+      setTodayVisitors(todayVisitorsCount);
+    }
+  }
+
+  useEffect(() => {
+    loadAllVisitors();
+    loadTodayVisitors();
+  }, []);
+
+  return {
+    allVisitors,
+    todayVisitors,
+  };
+};
